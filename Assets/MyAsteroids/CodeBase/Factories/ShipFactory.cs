582441af4de9748797034cf2d2fdec961aef7e8c
Roman Kopylov <R2070529@yandex.ru>
2024-08-18 19:21:57 +0300

    replaced DiContainer in ShipFactory

--- Assets/MyAsteroids/CodeBase/Factories/ShipFactory.cs
+++ Assets/MyAsteroids/CodeBase/Factories/ShipFactory.cs
@@ -5,16 +5,16 @@ namespace MyAsteroids.CodeBase.Factories
 {
     public class ShipFactory
     {
-        private readonly DiContainer _diContainer;
+        private readonly IInstantiator _instantiator;
         private Ship _shipPrefab;
         
-        public ShipFactory(DiContainer diContainer, Ship shipPrefab)
+        public ShipFactory(IInstantiator instantiator, Ship shipPrefab)
         {
-            _diContainer = diContainer;
+            _instantiator = instantiator;
             _shipPrefab = shipPrefab;
         }
         
         public Ship CreateShip() => 
-            _diContainer.InstantiatePrefabForComponent<Ship>(_shipPrefab);
+            _instantiator.InstantiatePrefabForComponent<Ship>(_shipPrefab);
     }
 }
